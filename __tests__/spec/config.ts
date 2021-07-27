75df22949568a953f7c32768e129379b724ba8ff
tkskto <tkskto@gmail.com>
2021-07-28 00:12:59 +0900

    feat: add test.

---
+++ __tests__/spec/config.ts
@@ -0,0 +1,35 @@
+import {convertConfigToJSON} from '../../src/Config';
+
+test('git config --list', async () => {
+  const configList = `credential.helper=osxkeychain
+user.name=tkskto
+user.email=tkskto@gmail.com
+core.excludesfile=/Users/tkskto/.gitignore_grobal
+core.repositoryformatversion=0
+core.filemode=true
+core.bare=false
+core.logallrefupdates=true
+core.ignorecase=true
+core.precomposeunicode=true
+remote.origin.url=https://github.com/tkskto/git-log-nodejs.git
+remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+branch.main.remote=origin
+branch.main.merge=refs/heads/main`.split('\n');
+  const config = await convertConfigToJSON(configList);
+
+  expect(config).toStrictEqual({
+    credential: { helper: 'osxkeychain' },
+    user: { name: 'tkskto', email: 'tkskto@gmail.com' },
+    core: {
+      excludesfile: '/Users/tkskto/.gitignore_grobal',
+      repositoryformatversion: '0',
+      filemode: 'true',
+      bare: 'false',
+      logallrefupdates: 'true',
+      ignorecase: 'true',
+      precomposeunicode: 'true'
+    },
+    remote: { origin: { url: 'https://github.com/tkskto/git-log-nodejs.git', fetch: '+refs/heads/*:refs/remotes/origin/*' } },
+    branch: { main: { remote: 'origin', merge: 'refs/heads/main'} }
+  });
+});
