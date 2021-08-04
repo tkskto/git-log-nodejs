d0c545f0aaff088623f32f4f146aa05d532a7368
tkskto <tkskto@gmail.com>
2021-08-04 23:43:34 +0900

    feat: update test.

--- __tests__/spec/commits.ts
+++ __tests__/spec/commits.ts
@@ -9,7 +9,18 @@ describe('commits test', () => {
     const logs = await commits(DEFAULT_LOG_OPTION);
     const first = logs[logs.length - 1];
 
-    if (!isCI){
+    if (isCI){
+      expect(first.hasOwnProperty('hash')).toBeTruthy();
+      expect(first.hasOwnProperty('date')).toBeTruthy();
+      expect(first.hasOwnProperty('parent')).toBeTruthy();
+      expect(first.hasOwnProperty('files')).toBeFalsy();
+
+      expect(first.hasOwnProperty('author')).toBeTruthy();
+      if (first.author) {
+        expect(first.author.hasOwnProperty('email')).toBeTruthy();
+        expect(first.author.hasOwnProperty('name')).toBeTruthy();
+      }
+    } else {
       expect(first).toStrictEqual({
         hash: 'f366fdf4e095ee08d733b54a1dc3eff81f3f075f',
         author: {
@@ -30,7 +41,22 @@ describe('commits test', () => {
     const logs = await commits(option);
     const first = logs[logs.length - 1];
 
-    if (!isCI) {
+    if (isCI) {
+      expect(first.hasOwnProperty('hash')).toBeTruthy();
+      expect(first.hasOwnProperty('date')).toBeTruthy();
+      expect(first.hasOwnProperty('parent')).toBeTruthy();
+
+      expect(first.hasOwnProperty('author')).toBeTruthy();
+      if (first.author) {
+        expect(first.author.hasOwnProperty('email')).toBeTruthy();
+        expect(first.author.hasOwnProperty('name')).toBeTruthy();
+      }
+
+      expect(first.hasOwnProperty('files')).toBeTruthy();
+      if (first.files) {
+        expect(first.files.length).toBeGreaterThan(0);
+      }
+    } else {
       expect(first).toStrictEqual({
         hash: 'f366fdf4e095ee08d733b54a1dc3eff81f3f075f',
         author: {
@@ -61,7 +87,18 @@ describe('commits test', () => {
     const logs = await commits(option);
     const first = logs[logs.length - 1];
 
-    if (!isCI) {
+    if (isCI){
+      expect(first.hasOwnProperty('hash')).toBeTruthy();
+      expect(first.hasOwnProperty('date')).toBeTruthy();
+      expect(first.hasOwnProperty('parent')).toBeTruthy();
+      expect(first.hasOwnProperty('files')).toBeFalsy();
+
+      expect(first.hasOwnProperty('author')).toBeTruthy();
+      if (first.author) {
+        expect(first.author.hasOwnProperty('email')).toBeTruthy();
+        expect(first.author.hasOwnProperty('name')).toBeTruthy();
+      }
+    } else {
       expect(first).toStrictEqual({
         hash: 'f366fdf4e095ee08d733b54a1dc3eff81f3f075f',
         author: {
