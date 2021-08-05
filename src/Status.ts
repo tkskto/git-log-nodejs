c2dfe298b67122b5d6443af5ffe5bb633da53cb4
tkskto <tkskto@gmail.com>
2021-08-06 00:20:35 +0900

    fix: fixed status.

--- src/Status.ts
+++ src/Status.ts
@@ -6,7 +6,7 @@ import {makeErrorMessage} from './ErrorLogFactory';
 export const status = async (): Promise<File[]> => {
   try {
     const result = await getFileStatus();
-    const files = result.split('\n');
+    const files = result.trim().split('\n');
 
     return parseFileData(files);
   } catch (err) {
